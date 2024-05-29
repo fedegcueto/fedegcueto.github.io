2096bc031de68ca32f09ce8a7b659de00e2aa759
fedegcueto <94404195+fedegcueto@users.noreply.github.com>
2024-05-29 05:53:36 -0300

    Update sketch.js

--- truco/sketch.js
+++ truco/sketch.js
@@ -53,7 +53,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowHeight * 0.70, windowHeight);
+  createCanvas(windowHeight * 0.60, windowHeight);
   initializeDeck();
   shuffleDeck();
   dealCards();
